6cba1a89c93aac6dabaeb8ece6ed488255029f2a
Saif Al-saedi <saif4004@gmail.com>
2024-09-25 15:26:30 -0700

    first client.js commit. Moved the connection fuction from play.js to client.js

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541,
+  });
+  conn.on('connect', () => {
+    console.log('Successfully connected to server');
+  });
+  conn.on('data', (data) => {
+    console.log('Server says: ', data.toString());
+  });
+  conn.on('error', (error) => {
+    console.error('An error occurred: ', error);
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = connect;
